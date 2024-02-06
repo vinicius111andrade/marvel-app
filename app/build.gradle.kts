acef9b44593ca71de842146f9cd87cadc848a253
Vinicius Andrade <vinicius111andrade@gmail.com>
2024-02-06 15:59:34 -0300

    Update sdk version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.vdemelo.marvel"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.vdemelo.marvel"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
