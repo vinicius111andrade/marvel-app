62b5f1c3d811250b1de67768c2a5e6032ce6ab25
Vinicius Andrade <vinicius111andrade@gmail.com>
2024-02-09 21:26:22 -0300

    Usecase to save selected char

--- app/src/main/java/com/vdemelo/marvel/domain/usecase/SelectedMarvelCharacterUseCase.kt
+++ app/src/main/java/com/vdemelo/marvel/domain/usecase/SelectedMarvelCharacterUseCase.kt
@@ -2,6 +2,7 @@ package com.vdemelo.marvel.domain.usecase
 
 import com.vdemelo.marvel.domain.model.MarvelCharacter
 
+//TODO ver se n tem um jeito melhor, pra deixar isso aqui na camada de UI só
 class SelectedMarvelCharacterUseCase {
     private var selectedCharacter: MarvelCharacter? = null
 
