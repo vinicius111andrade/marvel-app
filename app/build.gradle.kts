934d38c9d22be7c9f012aaa126f74c9d85776de7
Vinicius Andrade <vinicius111andrade@gmail.com>
2024-02-20 10:41:29 -0300

    Test character view model

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,6 +100,7 @@ dependencies {
     //Tests
     implementation("androidx.test.ext:junit-ktx:1.1.5")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     testImplementation("androidx.arch.core:core-testing:2.2.0")
